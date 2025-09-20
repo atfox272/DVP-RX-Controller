d0d2d661fd82703518e2e90f6c0482e6a5b5c9ca
atfox272 <trananhtai577@gmail.com>
2025-09-20 18:45:45 +0700

    mod: refactor grayscaler

--- rtl/drc_pxl_grayscaler.v
+++ rtl/drc_pxl_grayscaler.v
@@ -1,8 +1,10 @@
 // RGB565 -> GRAY 
 module drc_pxl_grayscaler
 #(
-    // Do not configure
     parameter RGB_PXL_W = 16,
+    parameter R_DAT_W   = 5,
+    parameter G_DAT_W   = 6,
+    parameter B_DAT_W   = 5,
     parameter GS_PXL_W  = 8
 )
 (
@@ -18,9 +20,6 @@ module drc_pxl_grayscaler
     input                   gs_pxl_rdy_i
 );
     // Localparam
-    localparam R_DAT_W      = 5;
-    localparam G_DAT_W      = 6;
-    localparam B_DAT_W      = 5;
     localparam R_MSB_IDX    = RGB_PXL_W-1;
     localparam G_MSB_IDX    = R_MSB_IDX-R_DAT_W;
     localparam B_MSB_IDX    = G_MSB_IDX-G_DAT_W;
@@ -35,8 +34,8 @@ module drc_pxl_grayscaler
     assign gs_pxl_last_o    = rgb_pxl_last_i;
     assign gs_pxl_vld_o     = rgb_pxl_vld_i;
     assign rgb_pxl_rdy_o    = gs_pxl_rdy_i;
-    assign r_data           = {rgb_pxl_i[R_MSB_IDX-:R_DAT_W], 3'b000};
-    assign g_data           = {rgb_pxl_i[G_MSB_IDX-:G_DAT_W], 2'b00};
-    assign b_data           = {rgb_pxl_i[B_MSB_IDX-:B_DAT_W], 3'b000};
+    assign r_data           = (STD_R_DAT_W)'(rgb_pxl_i[R_MSB_IDX-:R_DAT_W]);
+    assign g_data           = (STD_G_DAT_W)'(rgb_pxl_i[G_MSB_IDX-:G_DAT_W]);
+    assign b_data           = (STD_B_DAT_W)'(rgb_pxl_i[B_MSB_IDX-:B_DAT_W]);
     assign gs_pxl_o         = (r_data>>2) + (r_data>>5) + (g_data>>1) + (g_data>>4) + (b_data>>4) + (b_data>>5);
 endmodule
